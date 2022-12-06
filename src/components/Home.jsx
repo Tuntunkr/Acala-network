40fd3de13ec6f520ea6f46355672e52b1776df0e
Tuntunkr <tuntunkumar04051998@gmail.com>
2022-12-06 21:10:52 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Home.css"
+
+export default function Home() {
+  return (
+    <>
+    <section className="container-fluid">
+      <div className="conatiner">
+        <div className="Hero_heroContent">
+          <h1>The Decentralized Stablecoin of Polkadot</h1>
+          <p>Integrate your DApp with aUSD, the native decentralized stablecoin of Polkadot and Kusama.</p>
+
+          <div>
+          <a href="/articles" >EXPLORE</a> 
+        </div>
+        </div>
+        
+      </div>
+    </section>
+    </>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Free from "../components/Free";
+import Back from "../components/Back"
+import Deep from "../components/Deep"
+import Hero from "../components/Home";
+import ScrollToTop from "../components/ScrollToTop";
+import Latest from "../components/Latest"
+import "../sass/index.scss";
+
+function Home() {
+
+  return (
+   <>    
+      <Hero />
+
+      <ScrollToTop />
+      
+      {/* <Hero /> */}
+      <br></br>
+      <Latest/>
+      <Free />
+      <Back/>
+      <br></br>
+      <Deep/>
+      <Free />
+      <Footer />
+      </>
+
+  );
+}
+
+export default Home;
